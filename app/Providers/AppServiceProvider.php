0fb30f9792a7b2186896d7db739f132468db0c6d
gyan111 <4457406+gyan111@users.noreply.github.com>
2026-03-24 12:37:45 +0000

    Force https on server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,11 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->usePublicPath(__DIR__.'/../../public_html');
+        
+        // Force HTTPS URLs when accessed via HTTPS
+        if (request()->secure()) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
